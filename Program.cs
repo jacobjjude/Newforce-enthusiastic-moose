e20343f972d464674d8aa128167f3dd5a6279d79
Jacob Jude <jacobj.jude@outlook.com>
2022-09-12 13:26:01 -0400

    finished chap 2

--- Program.cs
+++ Program.cs
@@ -15,6 +15,14 @@ namespace enthusiastic_moose
             MooseSays("I AM REALLY ENTHUSIASTIC");
 
             //as a question
+            CanadaQuestion();
+            EnthusiasticQuestion();
+            LoveCSharpQuestion();
+            SecretQuestion();
+        }
+
+        static void CanadaQuestion()
+        {
             bool isTrue = MooseAsks("Is Canada Real?");
             if (isTrue)
             {
@@ -26,6 +34,44 @@ namespace enthusiastic_moose
             }
         }
 
+        static void EnthusiasticQuestion()
+{
+    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+    if (isEnthusiastic)
+    {
+        MooseSays("Yay!");
+    }
+    else
+    {
+        MooseSays("You should try it!");
+    }
+}
+
+static void LoveCSharpQuestion()
+{
+    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+    if (doesLoveCSharp)
+    {
+        MooseSays("Good job sucking up to your instructor!");
+    }
+    else
+    {
+        MooseSays("You will...oh, yes, you will...");
+    }
+}
+
+    static void SecretQuestion()
+    {
+        bool isEverythingFine = MooseAsks("Do you want to know a secret?");
+        if (isEverythingFine)
+        {
+         MooseSays("ME TOO!!!! I love secrets...tell me one!");
+        }
+    else
+    {
+        MooseSays("Oh, no...secrets are the best, I love to share them!");
+    }
+}
         static bool MooseAsks(string question)
         {
             Console.Write($"{question} (Y/N): ");

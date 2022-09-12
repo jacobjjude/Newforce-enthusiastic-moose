5e5aaf38b19ef98009744acfb79d013dd72c3718
Jacob Jude <jacobj.jude@outlook.com>
2022-09-12 13:20:46 -0400

    the moose asks a question now

--- Program.cs
+++ Program.cs
@@ -13,8 +13,39 @@ namespace enthusiastic_moose
             //Let the moose speak!
             MooseSays("H I, I'M E N T H U S I A S T I C !");
             MooseSays("I AM REALLY ENTHUSIASTIC");
+
+            //as a question
+            bool isTrue = MooseAsks("Is Canada Real?");
+            if (isTrue)
+            {
+                MooseSays("Really? It seems very unlikely");
+            }
+            else 
+            {
+                MooseSays("I  K N E W  I T !!!");
+            }
         }
 
+        static bool MooseAsks(string question)
+        {
+            Console.Write($"{question} (Y/N): ");
+            string answer = Console.ReadLine().ToLower();
+
+            while (answer != "y" && answer != "n")
+            {
+                Console.Write($"{question} (Y/N): ");
+                answer = Console.ReadLine().ToLower();
+            }
+
+            if (answer == "y")
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
         static void MooseSays(string message)
         {
             Console.WriteLine($@"

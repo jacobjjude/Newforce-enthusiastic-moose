a4edac3bed985ab6f9919f26c805baa3c1d529cc
Jacob Jude <jacobj.jude@outlook.com>
2022-09-12 11:30:08 -0400

    added customizable message to moose

--- Program.cs
+++ Program.cs
@@ -8,6 +8,43 @@ namespace enthusiastic_moose
         {
             Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
             Console.WriteLine("--------------------------------------------");
+            Console.WriteLine();
+
+            //Let the moose speak!
+            MooseSays("H I, I'M E N T H U S I A S T I C !");
+            MooseSays("I AM REALLY ENTHUSIASTIC");
+        }
+
+        static void MooseSays(string message)
+        {
+            Console.WriteLine($@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    {message}
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+            ");
         }
     }
 }
